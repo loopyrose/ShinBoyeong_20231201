f8f35704149b0ca3a9f77f6c0d92772c6bde5a3a
신보영 <loopyrose363@gamil.com>
2026-04-29 00:27:47 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -514,7 +514,7 @@ function moveEnemy() {
 
     let dx = x - enemy.x;
     let dy = y - enemy.y;
-    let espeed = 0.7+(1/d)*120;
+    let espeed = 0.7+(1/d)*150;
 
     enemy.x += (dx / d) * espeed;
     enemy.y += (dy / d) * espeed;
